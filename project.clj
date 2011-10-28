79d1c5cf28463ce2f5ea0b400762e73d6d59952f
J. Matt Peterson <jmatt@jmatt.org>
2011-10-28 15:38:10 -0700

    Added project.clj so I could push to clojars

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject jmatt/clojure.algo.generic "0.1.0-SNAPSHOT"
+  :description "clojure.algo.generic for Clojure 1.3.0. Example: (use 'clojure.algo.generic.math-functions)"
+  :url "https://github.com/clojure/algo.generic"
+  :dependencies  [[org.clojure/clojure "1.3.0"]
+                  [algo.generic "0.1.0-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojars "0.6.0"]])
