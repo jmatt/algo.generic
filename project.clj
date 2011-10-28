fa64f2887c434ac7553a28dbb8fc87b85cb2a331
J. Matt Peterson <jmatt@jmatt.org>
2011-10-28 15:40:24 -0700

    Update project name so it would match my clojars repo

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jmatt/clojure.algo.generic "0.1.0-SNAPSHOT"
+(defproject org.jmatt/clojure.algo.generic "0.1.0-SNAPSHOT"
   :description "clojure.algo.generic for Clojure 1.3.0. Example: (use 'clojure.algo.generic.math-functions)"
   :url "https://github.com/clojure/algo.generic"
   :dependencies  [[org.clojure/clojure "1.3.0"]
